f42ba1194234219b9a13248d5183252a4c05471d
zhangcheng <571346800@qq.com>
2020-12-02 22:09:37 +0800

    格式化

--- gulpfile.js
+++ gulpfile.js
@@ -14,85 +14,85 @@ const buildPath = './dist/';
 // 编译wxml文件
 const wxmlFiles = [`${srcPath}/*.wxml`];
 const wxml = () => {
-  return gulp.src(wxmlFiles, { since: gulp.lastRun(wxml) })
-    .pipe(gulp.dest(buildPath));
+    return gulp.src(wxmlFiles, { since: gulp.lastRun(wxml) })
+        .pipe(gulp.dest(buildPath));
 };
 gulp.task(wxml);
 
 // 编译less文件
 const lessFiles = [
-  `${srcPath}/*.less`,
-  `${srcPath}/*.wxss`
+    `${srcPath}/*.less`,
+    `${srcPath}/*.wxss`
 ];
 const isLess = (file) => {
-  return Object.is(file.extname, '.less');
+    return Object.is(file.extname, '.less');
 };
 const compileLess = () => {
-  return gulp.src(lessFiles, { since: gulp.lastRun(compileLess) })
-    .pipe(gulpif(isLess, less()))
-    .pipe(rename({ extname: '.wxss' }))
-    .pipe(gulp.dest(buildPath));
+    return gulp.src(lessFiles, { since: gulp.lastRun(compileLess) })
+        .pipe(gulpif(isLess, less()))
+        .pipe(rename({ extname: '.wxss' }))
+        .pipe(gulp.dest(buildPath));
 };
 gulp.task(compileLess);
 
 // 编译js文件
 const jsFiles = [`${srcPath}/*.js`, `!./src/env/*.js`];
 const js = () => {
-  return gulp.src(jsFiles, { since: gulp.lastRun(js) })
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(gulp.dest(buildPath));
+    return gulp.src(jsFiles, { since: gulp.lastRun(js) })
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(gulp.dest(buildPath));
 };
 gulp.task(js);
 
 // 编译json文件
 const jsonFiles = [`${srcPath}/*.json`];
 const json = () => {
-  return gulp.src(jsonFiles, { since: gulp.lastRun(json) })
-    .pipe(gulp.dest(buildPath));
+    return gulp.src(jsonFiles, { since: gulp.lastRun(json) })
+        .pipe(gulp.dest(buildPath));
 };
 gulp.task(json);
 
 // 编译图片
 const imgFiles = [
-  `${srcPath}/images/**/*.*`
+    `${srcPath}/images/**/*.*`
 ];
 const img = () => {
-  return gulp.src(imgFiles, { since: gulp.lastRun(img) })
-    .pipe(imagemin())
-    .pipe(gulp.dest(buildPath))
+    return gulp.src(imgFiles, { since: gulp.lastRun(img) })
+        .pipe(imagemin())
+        .pipe(gulp.dest(buildPath))
 };
 gulp.task(img);
 
 // 清除build目录下的所有文件
 gulp.task('clean', done => {
-  del.sync(['dist/**/*']);
-  done();
+    del.sync(['dist/**/*']);
+    done();
 });
 
 // 监听文件
 gulp.task('watch', () => {
-  gulp.watch(wxmlFiles, wxml);
-  gulp.watch([...lessFiles], compileLess);
-  gulp.watch(jsFiles, js);
-  gulp.watch(jsonFiles, json);
-  gulp.watch(imgFiles, img);
+    gulp.watch(wxmlFiles, wxml);
+    gulp.watch([...lessFiles], compileLess);
+    gulp.watch(jsFiles, js);
+    gulp.watch(jsonFiles, json);
+    gulp.watch(imgFiles, img);
 });
 
 // development环境
 gulp.task('dev',
-  gulp.series(
-    'clean',
-    gulp.parallel('wxml', 'compileLess', 'js', 'json', 'img'),
-    'watch'
-  )
+    gulp.series(
+        'clean',
+        gulp.parallel('wxml', 'compileLess', 'js', 'json', 'img'),
+        'watch'
+    )
 )
 
 // production环境
 gulp.task('build',
-  gulp.series(
-    'clean',
-    gulp.parallel('wxml', 'compileLess', 'js', 'json', 'img'),
-    'watch'
-  )
+    gulp.series(
+        'clean',
+        gulp.parallel('wxml', 'compileLess', 'js', 'json', 'img'),
+        'watch'
+    )
 )
