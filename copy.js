1b5cc863d232958061e89948ba2c4c34ce16d862
zhangcheng <571346800@qq.com>
2020-12-24 00:11:11 +0800

    1

--- copy.js
+++ copy.js
@@ -12,79 +12,80 @@ const aliases = require('gulp-wechat-weapp-src-alisa'); // 别名模块
 const srcPath = 'src/**/';
 const distPath = 'dist/';
 const filePath = {
-    wxmlFiles: [`${srcPath}*.wxml`],
-    jsonFiles: [`${srcPath}*.json`],
-    lessFiles: [`${srcPath}*.less`, `${srcPath}*.wxss`],
-    jsFiles: [`${srcPath}*.js`],
-    imgFiles: [`${srcPath}/assets/images/**/*.*`]
+  wxmlFiles: [`${srcPath}*.wxml`],
+  jsonFiles: [`${srcPath}*.json`],
+  lessFiles: [`${srcPath}*.less`, `${srcPath}*.wxss`],
+  jsFiles: [`${srcPath}*.js`],
+  imgFiles: [`${srcPath}/assets/images/**/*.*`]
 }
 
-// 直接复制 wxml
-function dealWxml() {
-    return gulp.src(filePath.wxmlFiles)
+// wxml【别名、复制】
+function dealWxml () {
+  return gulp.src(filePath.wxmlFiles)
     .pipe(aliases({ '@': 'src' }))
     .pipe(gulp.dest(distPath));
 }
 
-// 直接复制 json
-function dealJson() {
+// json【直接复制】
+function dealJson () {
   return gulp.src(filePath.jsonFiles)
-      .pipe(gulp.dest(distPath));
+    .pipe(gulp.dest(distPath));
 }
 
-// 编译less文件
-function dealLess() {
-    return gulp.src(filePath.lessFiles)
-        .pipe(aliases({ '@': 'src' }))
-        .pipe(px2rpx({
-            screenWidth: 375,
-            wxappScreenWidth: 750,
-            remPrecision: 6
-        }))
-        .pipe(rename({ extname: '.wxss' }))
-        .pipe(gulp.dest(distPath));
+// less【编译成wxss、别名、px2rpx、复制】
+function dealLess () {
+  return gulp.src(filePath.lessFiles)
+    .pipe(less())
+    .pipe(rename({ extname: '.wxss' }))
+    .pipe(aliases({ '@': 'src' }))
+    .pipe(px2rpx({
+      screenWidth: 375,
+      wxappScreenWidth: 750,
+      remPrecision: 6
+    }))
+    .pipe(gulp.dest(distPath));
 }
 
-// js加个eslint验证、路径别名
-function dealJs() {
-    return gulp.src(filePath.jsFiles)
-        .pipe(eslint())
-        .pipe(aliases({ '@': 'src' }))
-        .pipe(eslint.format())
-        .pipe(gulp.dest(distPath));
+// js【eslint、别名】
+function dealJs () {
+  return gulp.src(filePath.jsFiles)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(aliases({ '@': 'src' }))
+    .pipe(gulp.dest(distPath));
 }
 
-// 压缩图片
-function dealImg() {
-    return gulp.src(filePath.imgFiles)
+// img【压缩】
+function dealImg () {
+  return gulp.src(filePath.imgFiles)
     .pipe(imagemin({
-        interlaced: true, //隔行扫描压缩jqp图片
-        optimizationLevel: 5, //0-7
-        progressive: true, //无损压缩jpg
-        multipass: true //多次优化svg直到最优
+      interlaced: true, //隔行扫描压缩jqp图片
+      optimizationLevel: 5, //0-7
+      progressive: true, //无损压缩jpg
+      multipass: true //多次优化svg直到最优
     }))
     .pipe(gulp.dest(distPath))
 }
 
 // 清除dist目录
-function cleanDist() {
+function cleanDist () {
   return del(distPath);
 }
 
 // 监听文件
-function watchFiles() {
-    gulp.watch(filePath.wxmlFiles, dealWxml);
-    gulp.watch(filePath.lessFiles, dealLess);
-    gulp.watch(filePath.jsFiles, dealJs);
-    gulp.watch(filePath.imgFiles, dealImg);
-    gulp.watch(filePath.jsonFiles, dealJson);
+function watchFiles () {
+  gulp.watch(filePath.wxmlFiles, dealWxml);
+  gulp.watch(filePath.lessFiles, dealLess);
+  gulp.watch(filePath.jsFiles, dealJs);
+  gulp.watch(filePath.imgFiles, dealImg);
+  gulp.watch(filePath.jsonFiles, dealJson);
 }
 
 // 默认
 gulp.task('default',
-    gulp.series(
-        cleanDist,
-        gulp.parallel(dealWxml, dealLess, dealJs, dealImg, dealJson),
-        watchFiles
-    )
+  gulp.series(
+    cleanDist,
+    gulp.parallel(dealWxml, dealLess, dealJs, dealImg, dealJson),
+    watchFiles
+  )
 )
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,50 @@
 // 初始化模块
 const gulp = require('gulp');
+const less = require('gulp-less'); // 编译less
+const del = require('del'); // 删除
+const rename = require('gulp-rename'); // 重命名模块
+const eslint = require('gulp-eslint'); // eslint
+const imagemin = require('gulp-imagemin'); // 压缩图片模块
+const px2rpx = require('gulp-px2rpx'); // px转rpx
+const aliases = require('gulp-wechat-weapp-src-alisa'); // 别名模块
 
 // 定义一些路径
 const srcPath = '';
 const distPath = '';
 const filePath = {
-    wxmlFiles: [],
-    jsonFiles: [],
-    lessFiles: [, ],
-    jsFiles: [],
-    imgFiles: []
+  wxmlFiles: [],
+  jsonFiles: [],
+  lessFiles: [,],
+  jsFiles: [],
+  imgFiles: []
 }
 
-// 直接复制 wxml
-function dealWxml() {
+// wxml【别名、复制】
+function dealWxml () {
 }
 
-// 直接复制 json
-function dealJson() {
+// json【直接复制】
+function dealJson () {
 }
 
-// 编译less文件
-function dealLess() {
+// less【编译成wxss、别名、px2rpx、复制】
+function dealLess () {
 }
 
-// js加个eslint验证、路径别名
-function dealJs() {
+// js【eslint、别名】
+function dealJs () {
 }
 
-// 压缩图片
-function dealImg() {
+// img【压缩】
+function dealImg () {
 }
 
 // 清除dist目录
-function cleanDist() {
+function cleanDist () {
 }
 
 // 监听文件
-function watchFiles() {
+function watchFiles () {
 }
 
 // 默认
